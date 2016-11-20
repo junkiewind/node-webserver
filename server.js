443fd2e9e4c1110e594433d38ad2d5b4fc52ea3d
Alexander Klimov <alexander@subsystem.com.ua>
2016-11-20 16:19:46 +0200

    Initial commit

---
+++ server.js
@@ -0,0 +1,56 @@
+const express = require('express'),
+      hbs = require('hbs'),
+      fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials/')
+app.set('view engine', 'hbs');
+
+app.use((req, res, next) => {
+  var now = new Date().toString(),
+      log = `---- ${now}: ${req.method} ${req.url}`;
+  fs.appendFile('server.log', log + '\n', (err) => {
+    if (err) {
+      console.log('Unable to append to server.log');
+    }
+  });
+  console.log();
+  next();
+});
+
+// app.use((req, res, next) => {
+//   res.render('maintenance.hbs');
+// });
+
+app.use(express.static(__dirname + '/public/'));
+
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear();
+});
+hbs.registerHelper('screamIt', (text) => {
+  return text.toUpperCase();
+});
+
+
+app.get('/', (req, res) => {
+  res.render('home.hbs', {
+    pageTitle: 'NodeJS Course',
+    welcomeMsg: 'Welcome to dynamic page',
+  });
+});
+app.get('/about', (req, res) => {
+  res.render('about.hbs', {
+    pageTitle: 'Dynamic content about page',
+  });
+});
+app.get('/bad', (req, res) => {
+  res.send({
+    errorMessage: 'Something goes wrong :-('
+  });
+});
+
+
+app.listen(3000, () => {
+  console.log('****\nNode-server is up on port 3000\n****');
+});
